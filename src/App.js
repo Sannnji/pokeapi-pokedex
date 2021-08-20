edea9ff9323d054d839e925abecad6de66f9b9e1
Sannnji <jamesji.penfield@protonmail.com>
2021-08-19 22:21:51 -0400

    netlify fix

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { Flex } from "@chakra-ui/layout";
 import { Layout } from "./components/Layout";
 import PokeList from "./components/pokeList/PokeList";
 import PokeInfo from "./components/pokeInfo/PokeInfo";
-import { PokeProvider } from "./Context";
+import PokeProvider  from "./Context";
 
 function App() {
   return (
