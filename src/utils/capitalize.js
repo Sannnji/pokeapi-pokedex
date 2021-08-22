c382c69f6acf0219c5f27d7ebf3c6d2c0e379640
Sannnji <jamesji.penfield@protonmail.com>
2021-08-22 16:39:10 -0400

    updated script to capitalized sentences

--- src/utils/capitalize.js
+++ src/utils/capitalize.js
@@ -1,5 +1,16 @@
 const capitalize = (string) => {
-  const capitalized = string.charAt(0).toUpperCase() + string.slice(1);
+  let capitalized;
+  var words = string.split(" ");
+
+  if (words.length <= 1) {
+    capitalized = string[0].toUpperCase() + string.slice(1);
+  } else if (words.length > 1) {
+    for (let i = 0; i < words.length; i++) {
+      words[i] = words[i][0].toUpperCase() + words[i].substr(1);
+    }
+    capitalized = words.join(" ");
+  }
+
   return capitalized;
 };
 
