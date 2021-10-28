1413b81ca12157413aa9c1b994b8c7c98f878d7f
Sannnji <jamesji.penfield@protonmail.com>
2021-10-28 17:39:46 -0400

    made search icon visable

--- src/components/SeachBar.jsx
+++ src/components/SeachBar.jsx
@@ -3,11 +3,7 @@ import { Input, InputGroup, InputLeftElement } from "@chakra-ui/input";
 const SearchBar = ({ setSearchValue }) => {
   return (
     <InputGroup borderColor="#CBD5E0">
-      <InputLeftElement
-        ml={2}
-        color="white"
-        children={<i className="fas fa-search" />}
-      />
+      <InputLeftElement ml={2} children={<i className="fas fa-search" />} />
       <Input onChange={(event) => setSearchValue(event.target.value)} />
     </InputGroup>
   );
