e4090114f45d4a0e4d12970f481d10e0a21dd1ef
Sannnji <jamesji.penfield@protonmail.com>
2022-05-14 19:03:30 -0400

    removed context and added route for individual poke query

--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@ import PokeList from "./components/PokeList";
 
 import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
-import { PokeProvider } from "./context";
 import theme from "./theme";
+import PokeEntry from "./components/pokeEntry/PokeEntry";
 
 const client = new ApolloClient({
   // uri: "http://localhost:9000/",
@@ -25,18 +25,20 @@ const client = new ApolloClient({
 ReactDOM.render(
   <ApolloProvider client={client}>
     <ChakraProvider theme={theme}>
-      <PokeProvider>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<App />}>
-              <Route index element={<PokeList />} />
-              <Route path=":genId" element={<PokeList />} />
-              <Route path=":genId/:pokeType" element={<PokeList />} />
-            </Route>
-          </Routes>
-        </BrowserRouter>
-        <ColorModeScript />
-      </PokeProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />}>
+            <Route index element={<PokeList />} />
+            <Route path=":genId" element={<PokeList />} />
+            <Route path=":genId/:pokeType" element={<PokeList />} />
+            <Route
+              path={"/pokedex/:pokeId"}
+              element={<PokeEntry isOpen={true} />}
+            />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+      <ColorModeScript />
     </ChakraProvider>
   </ApolloProvider>,
   document.getElementById("root")
